c78832c8b99c6991b76a6cd451883de2e0cd9a5f
Pete <pbianchi192@gmail.com>
2018-02-25 12:15:12 -0500

    First commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/completedForm', methods=['POST'])
+def completedForm():
+    name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
+    return render_template('form.html', name=name, location=location, language=language, comment=comment)
+
+app.run(debug = True)
